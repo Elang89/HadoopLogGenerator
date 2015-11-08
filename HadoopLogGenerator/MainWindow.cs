64e6aa51b3c1b1dd99ae947c91d8c244f6d1281b
Elang89 <elang8899@gmail.com>
2015-11-08 02:02:34 -0600

    Cambios a generador de logs

--- HadoopLogGenerator/MainWindow.cs
+++ HadoopLogGenerator/MainWindow.cs
@@ -26,42 +26,60 @@ namespace HadoopLogGenerator
         private void createLogButton_Click(object sender, EventArgs e)
         {
 
-            this.createLogButton.Enabled = false;
-            Task task1 = Task.Factory.StartNew(() => insertLogs(1, 20000));
-            Task task2 = Task.Factory.StartNew(() => insertLogs(2, 20000));
-            Task task3 = Task.Factory.StartNew(() => insertLogs(3, 20000));
-            Task task4 = Task.Factory.StartNew(() => insertLogs(4, 20000));
+            Task task1 = Task.Factory.StartNew(() => insertLogs(1));
+            //Task task2 = Task.Factory.StartNew(() => insertLogs(2));
+            //Task task3 = Task.Factory.StartNew(() => insertLogs(3));
 
+            this.createLogButton.Enabled = false;
+
+            /*if (task1.IsCompleted && task2.IsCompleted && task3.IsCompleted)
+            {
+                this.createLogButton.Enabled = false;
+            }*/
 
         }
 
-        private void insertLogs(int id, int cycleSleepTime)
+        private void insertLogs(int id)
         {
+            
             HttpClient client = new HttpClient();
+            client.Timeout = TimeSpan.FromSeconds(360);
             client.BaseAddress = new Uri("http://localhost:49496");
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            LogGeneratorObject logObject = new LogGeneratorObject();
             StringContent jsonContent;
             HttpResponseMessage response;
-
-            for (int i = 0; i < 125000; i++)
+            try
             {
-                try
-                {
-                    Console.WriteLine("task {0} has begun inserting log.", id);
-                    JObject jObject = logObject.generateJson();
-                    string json = JsonConvert.SerializeObject(jObject);
-                    jsonContent = new StringContent(json, Encoding.UTF8, "application/json");
-                    response = client.PostAsync("/Logs/Create", jsonContent).Result;
-                    Console.WriteLine("task {0} has inserted log", id);
-                }
-                catch (Exception error)
-                {
-                    Console.WriteLine(error.ToString());
-                }
+                string json = buildJsonLog(id);
+                jsonContent = new StringContent(json, Encoding.UTF8, "application/json");
+                response = client.PostAsync("/Logs/Create", jsonContent).Result;
+                var contents = response.Content.ReadAsStringAsync();
+                Console.WriteLine(contents.Result);
+                Console.WriteLine("Task {0} finished", id);
             }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.ToString());
+            }
+        }
+
+        private string buildJsonLog(int id)
+        {
+            LogGeneratorObject logObject = new LogGeneratorObject();
+            JObject jObject;
+            JArray finalObject = new JArray();
+            string jsonLog;
+            for (int i = 0; i < 500000; i++)
+            {
+                Console.WriteLine("Task {0} inserting log number " + i.ToString(), id);
+                Console.WriteLine("----------------------------");
+                jObject = logObject.generateJson();
+                finalObject.Add(jObject);
+            }
+            jsonLog = JsonConvert.SerializeObject(finalObject);
+            return jsonLog;
         }
     }
 }
